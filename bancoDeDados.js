db053c7c386d084eb9148ffe9f287ee1f0f01f72
mariasantos-j <jocyaramaria@gmail.com>
2024-11-11 00:07:15 -0300

    insere versao do projeto com integracao ao mongodb 2

--- bancoDeDados.js
+++ bancoDeDados.js
@@ -6,7 +6,7 @@ async function conectaBancoDeDados() {
     try {
         console.log('Conexão com Banco de Dados iniciou')
 
-    await mongoose.connect('process.env.MONGO_URL')
+    await mongoose.connect(process.env.MONGO_URL)
 
     console.log('Conexão com o Banco de Dados feita com sucesso!')
     } catch(erro) {
